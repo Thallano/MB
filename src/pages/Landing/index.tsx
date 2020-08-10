1983872264e93cc6a604454c1f1181fd1d742ac2
Thallano <cristyanthallano@gmail.com>
2020-08-10 14:28:06 -0300

    MB

---
+++ src/pages/Landing/index.tsx
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from 'react';
+import { View, Image, Text } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { RectButton } from 'react-native-gesture-handler';
+
+
+import styles from './styles';
+
+
+function Landing(){
+    const { navigate } = useNavigation();
+
+}
+
+export default Landing;
